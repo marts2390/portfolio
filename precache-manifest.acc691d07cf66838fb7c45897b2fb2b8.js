f3f7b78b250f8c23c78029ad40706c78e76ab0ab
Chris Martyr <chrismartyr90@gmail.com>
2020-04-05 16:15:08 +0100

    Updates

---
+++ precache-manifest.acc691d07cf66838fb7c45897b2fb2b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee0470aad52ef62bd471fcc40e30aa6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d51797a5bc8621758cce",
+    "url": "/portfolio/static/css/main.cd87de0d.chunk.css"
+  },
+  {
+    "revision": "8d81851806e604e405f8",
+    "url": "/portfolio/static/js/2.c4c5ee2c.chunk.js"
+  },
+  {
+    "revision": "4ec683f3dcd09d77d5902a3fd17d0144",
+    "url": "/portfolio/static/js/2.c4c5ee2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d51797a5bc8621758cce",
+    "url": "/portfolio/static/js/main.23532cad.chunk.js"
+  },
+  {
+    "revision": "1ebc751d0c6eacfaae87",
+    "url": "/portfolio/static/js/runtime-main.ae6c1a79.js"
+  },
+  {
+    "revision": "5e02e74b66e4cb6eabd82e6d98977a81",
+    "url": "/portfolio/static/media/affpodcast_logo.5e02e74b.png"
+  },
+  {
+    "revision": "608f0557e3d65a525716cd4c2e0781a2",
+    "url": "/portfolio/static/media/fred_hills_music.608f0557.png"
+  },
+  {
+    "revision": "4a886f7fdad920c21622e6353bfa7ba5",
+    "url": "/portfolio/static/media/indecision.4a886f7f.png"
+  },
+  {
+    "revision": "03a729d268da165d0ce596ba1e6d5418",
+    "url": "/portfolio/static/media/jobboard.03a729d2.png"
+  },
+  {
+    "revision": "4a22bd7c0b3edca3e592b987d48be344",
+    "url": "/portfolio/static/media/remote-banner.4a22bd7c.jpg"
+  },
+  {
+    "revision": "7fd624e448c93b5eeed12b05b5b43993",
+    "url": "/portfolio/static/media/statsinsider_logo_white.7fd624e4.png"
+  },
+  {
+    "revision": "77b0033e790e1a0d9cc0112bce7bd9c1",
+    "url": "/portfolio/static/media/taskcardapp.77b0033e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marts2390/portfolio/precache-manifest.f778833ea08b4336ea41098a8d319f04.js"
+  "/portfolio/precache-manifest.acc691d07cf66838fb7c45897b2fb2b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marts2390/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
