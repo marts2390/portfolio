2bca882058ed45c0f4a73be7b1027d703ba039fa
Chris Martyr <chrismartyr90@gmail.com>
2020-04-05 15:54:55 +0100

    Updates

---
+++ precache-manifest.3c328ce28030ac689f42878a2cb824c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedb52c9ca03e0ec927e70299e3f32ad",
+    "url": "/marts2390/portfolio.git/index.html"
+  },
+  {
+    "revision": "d51797a5bc8621758cce",
+    "url": "/marts2390/portfolio.git/static/css/main.cd87de0d.chunk.css"
+  },
+  {
+    "revision": "8ae8a6d5acc38b4ccf7b",
+    "url": "/marts2390/portfolio.git/static/js/2.a442eb7a.chunk.js"
+  },
+  {
+    "revision": "4ec683f3dcd09d77d5902a3fd17d0144",
+    "url": "/marts2390/portfolio.git/static/js/2.a442eb7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d51797a5bc8621758cce",
+    "url": "/marts2390/portfolio.git/static/js/main.23532cad.chunk.js"
+  },
+  {
+    "revision": "355f67aebccd994ddb94",
+    "url": "/marts2390/portfolio.git/static/js/runtime-main.2ddc92bf.js"
+  },
+  {
+    "revision": "5e02e74b66e4cb6eabd82e6d98977a81",
+    "url": "/marts2390/portfolio.git/static/media/affpodcast_logo.5e02e74b.png"
+  },
+  {
+    "revision": "608f0557e3d65a525716cd4c2e0781a2",
+    "url": "/marts2390/portfolio.git/static/media/fred_hills_music.608f0557.png"
+  },
+  {
+    "revision": "4a886f7fdad920c21622e6353bfa7ba5",
+    "url": "/marts2390/portfolio.git/static/media/indecision.4a886f7f.png"
+  },
+  {
+    "revision": "03a729d268da165d0ce596ba1e6d5418",
+    "url": "/marts2390/portfolio.git/static/media/jobboard.03a729d2.png"
+  },
+  {
+    "revision": "4a22bd7c0b3edca3e592b987d48be344",
+    "url": "/marts2390/portfolio.git/static/media/remote-banner.4a22bd7c.jpg"
+  },
+  {
+    "revision": "7fd624e448c93b5eeed12b05b5b43993",
+    "url": "/marts2390/portfolio.git/static/media/statsinsider_logo_white.7fd624e4.png"
+  },
+  {
+    "revision": "77b0033e790e1a0d9cc0112bce7bd9c1",
+    "url": "/marts2390/portfolio.git/static/media/taskcardapp.77b0033e.png"
+  }
+]);
